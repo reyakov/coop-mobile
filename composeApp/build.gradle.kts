da447ac4479217a15fd36969cead022a1e74f7e2
Ren Amamiya <reyakov@proton.me>
2026-05-23 20:39:16 +0700

    add proguard rules

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -79,7 +79,8 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt")
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
             )
             signingConfig = signingConfigs.getByName("release")
         }
