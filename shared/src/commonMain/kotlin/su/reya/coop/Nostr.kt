d8278002e7bb86070b4cd937a4b9c937903cae49
Ren Amamiya <reyakov@proton.me>
2026-06-03 14:47:41 +0700

    fix crash when get all cached metadata

--- shared/src/commonMain/kotlin/su/reya/coop/Nostr.kt
+++ shared/src/commonMain/kotlin/su/reya/coop/Nostr.kt
@@ -505,13 +505,18 @@ class Nostr {
             val results = mutableMapOf<PublicKey, Metadata>()
 
             events?.toVec()?.forEach { event ->
-                val metadata = Metadata.fromJson(event.content())
-                results[event.author()] = metadata
+                try {
+                    val metadata = Metadata.fromJson(event.content())
+                    results[event.author()] = metadata
+                } catch (e: Exception) {
+                    println("Failed to parse metadata: $e")
+                }
             }
 
             return results
         } catch (e: Exception) {
-            throw IllegalStateException("Failed to get cache metadata: ${e.message}", e)
+            println("Failed to get all cache metadata: ${e.message}")
+            return emptyMap()
         }
     }
 
