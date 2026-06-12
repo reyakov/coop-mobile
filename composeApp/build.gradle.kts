1f966de654f1f68536d955cdfaaa592fbc63a993
Ren Amamiya <reyakov@proton.me>
2026-06-12 15:50:14 +0700

    chore: bump version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 1
-        versionName = "0.1.8"
+        versionName = "0.1.9"
     }
     packaging {
         resources {
